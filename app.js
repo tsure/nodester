9068387c67b83bb8e2e45e02b3887e4b4e457ba5
Tanveer Sure <tanveersure@Tanveers-MacBook-Pro.local>
2015-01-21 10:29:42 +0530

    Commiting with sign up and login feature

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var restify = require('restify');
 var mongojs = require("mongojs");
-var users = require('login-mongo');
+var bcrypt = require('bcrypt');
+
+console.log("logging into database..");
+var db = mongojs('mongodb://taxiapp:taxiTaxi!@ds050077.mongolab.com:50077/taxiappservice', ['users']);
+var users = db.collection("users");
+console.log("db = "+db);
+//var jobs = db.collection("jobs");
+db.on('error',function(err) {
+    console.log('database error', err);
+});
+
+db.on('ready',function() {
+    console.log('database connected');
+});
+
 
 var ip_addr = '127.0.0.1';
 var port    =  '3000';
@@ -17,9 +31,8 @@ server.listen(port ,ip_addr, function(){
     console.log('%s listening at %s ', server.name , server.url);
 });
 
-var connection_string = '127.0.0.1:27017/taxiAppService';
-var db = mongojs(connection_string, ['taxiAppService']);
-var jobs = db.collection("jobs");
+
+
 
 var PATH = '/jobs'
 server.get({path : PATH , version : '0.0.1'} , findAllJobs);
@@ -31,27 +44,55 @@ var LOGINPATH = '/users'
 server.post({path:LOGINPATH + '/createuser'} , addUser);
 server.post({path:LOGINPATH + '/logintry'}, checkUserPassword);
 
-function addUser(req, res) {
+function addUser(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin','*');
-	users.add(req.body.email, req.body.user, req.body.pass, function(err, success) {
-    return res.end(JSON.stringify({
-      error: err,
-      success: success
-    }));
-  });
+    var emailId = req.body.email;
+    var firstName = req.body.firstname;
+    var lastName = req.body.lastname;
+    var password = req.body.password;
+    console.log("password = "+password);
+
+    var salt = bcrypt.genSaltSync(10);
+    var passHash = bcrypt.hashSync(password, salt);
+    
+    console.log("password hash = "+passHash);
+    //Store details in database
+    var user = {};
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = emailId;
+    user.password = passHash; //storing the password hash and not the password..
+
+
+    users.save(user , function(err , success){
+        console.log('Response success '+success);
+        console.log('Response error '+err);
+        if(success){
+            res.send(201 , user);
+            return next();
+        }else{
+            return next(err);
+        }
+    });
+
+
+    
 }
 
-function checkUserPassword(req, res) {
+function checkUserPassword(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin','*');
-	users.checkPassword(req.body.user, req.body.pass, function(success) {
-    if (success) {
-      req.session.user = req.body.user;
-      return res.redirect('/fareride.html');
-    } else {
-      req.session.user = void 0;
-      return res.redirect('/sign-in.html');
-    }
-  });
+    var foundUser = users.findOne({
+        email: req.body.email,
+    },function(err, doc) {
+        var foundUserPassword = doc.password;
+        var userPasswordMatches = bcrypt.compareSync(req.body.password, foundUserPassword);
+        if(userPasswordMatches){
+            res.send(200 , doc);
+            return next();
+        }else{
+            return next(err);
+        }
+    });
 }
 
 
@@ -119,3 +160,21 @@ function deleteJob(req , res , next){
     })
  
 }
+
+function genSaltForPassword(password) {
+    var passHash = null;
+    bcrypt.genSalt(10, function(err, salt) {
+    console.log("password in gensalt= "+password);        
+    bcrypt.hash(password, salt, function(err, hash) {
+            console.log("hash generated = "+hash);
+            passHash =  hash;
+        });
+    });
+    return passHash;
+}
+
+function comparePasswordSalt(password) {
+    bcrypt.compare(password, hash, function(err, res) {
+        return res;
+    });
+} 
